cb87025bf08bfd28a7478f9f61e52f40160a08b0
Stephen Mark Smith <smsmith195@gmail.com>
2024-04-05 11:35:39 +0100

    Optimise working Triangle App scripts

--- Triangle Test Cases/TriangleTestCase01.py
+++ Triangle Test Cases/TriangleTestCase01.py
@@ -1,19 +1,24 @@
 # The intent of this test case is to use a single variable to create an equilateral triangle
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
-driver = webdriver.Firefox()
-driver.maximize_window()
-driver.get("https://testpages.herokuapp.com/styled/apps/triangle/triangle001.html")
-driver.implicitly_wait(10)
+def set_up_page(driver):
+    driver.get("https://testpages.herokuapp.com/styled/apps/triangle/triangle001.html")
 
-sideLength = 5
+def create_triangle(driver):
+    sideLength = 5
+    driver.find_element(By.NAME, "side1").send_keys(sideLength)
+    driver.find_element(By.NAME, "side2").send_keys(sideLength)
+    driver.find_element(By.NAME, "side3").send_keys(sideLength)
+    driver.find_element(By.ID, "identify-triangle-action").click()
 
-driver.find_element(By.NAME, "side1").send_keys(sideLength)
-driver.find_element(By.NAME, "side2").send_keys(sideLength)
-driver.find_element(By.NAME, "side3").send_keys(sideLength)
+def main():
+    driver = webdriver.Firefox()
+    set_up_page(driver)
+    create_triangle(driver)
+    time.sleep(5)
+    driver.quit()
 
-driver.find_element(By.ID, "identify-triangle-action").click()
-
-driver.quit()
+if __name__ == "__main__":
+    main()
--- Triangle Test Cases/TriangleTestCase03.py
+++ Triangle Test Cases/TriangleTestCase03.py
@@ -2,19 +2,26 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
-driver = webdriver.Firefox()
-driver.maximize_window()
-driver.get("https://testpages.herokuapp.com/styled/apps/triangle/triangle001.html")
-driver.implicitly_wait(10)
 
-sideLengthA = 5
-sideLengthB = 8
+def create_triangle(driver):
+    side_length_a = 5
+    side_length_b = 8
 
-driver.find_element(By.NAME, "side1").send_keys(sideLengthA)
-driver.find_element(By.NAME, "side2").send_keys(sideLengthB)
-driver.find_element(By.NAME, "side3").send_keys(sideLengthA)
+    driver.find_element(By.NAME, "side1").send_keys(side_length_a)
+    driver.find_element(By.NAME, "side2").send_keys(side_length_b)
+    driver.find_element(By.NAME, "side3").send_keys(side_length_a)
+    driver.find_element(By.ID, "identify-triangle-action").click()
 
-driver.find_element(By.ID, "identify-triangle-action").click()
 
-driver.quit()
+def main():
+    driver = webdriver.Firefox()
+    driver.get("https://testpages.herokuapp.com/styled/apps/triangle/triangle001.html")
+    create_triangle(driver)
+    time.sleep(5)
+    driver.quit()
+
+
+if __name__ == "__main__":
+    main()
